c08e403a244319f373189aad503ff3e93ebc2719
salahahmed2001 <salah01068441@gmail.com>
2025-01-24 03:10:24 +0200

    add type

--- src/app/blog/[[...slug]]/page.tsx
+++ src/app/blog/[[...slug]]/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Blog = ({params}:{params:{slug:string}}) => {
+const Blog = ({params}:{params:{slug:string[];}}) => {
     if(params.slug?.length === 2){
         return <h1> Blog for category {params.slug[0]} for title{params.slug[1]}</h1>
     }else if(params.slug?.length === 1){
@@ -14,3 +14,4 @@ const Blog = ({params}:{params:{slug:string}}) => {
 }
 
 export default Blog
+
